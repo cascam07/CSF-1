d3376ace192dd47273a53e2dfe915eba0dbba986
Bartels <barrob16@ccm-p53.evergreen.edu>
2013-10-10 11:18:40 -0700

    hw2_test Pre-Lab update

---
+++ hw2_test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Test file for hw2.py
+
+# Compute the following value:
+#  1 + 1/1! + 1/2! + 1/3! + 1/4! + ... + 1/10!
+# The value should be close to e (≈ 2.71828), the base of the natural logarithms.
+# Hint: The easiest way to solve this is with two nested for loops. It is possible, but tricky, to compute this using only one for loop. That is not necessary for this assignment.
+# Hint: Copy your solution to "Problem 5: Multiple factorials", then modify it. Rather than printing the factorials, you will add their reciprocals to a running total, then print that total at the end.
+# Hint: don't try to work the very first "1 +" into your loop; do it outside the loops (either at the very beginning or the very end of the outer loop).
+
+
+n = 10
+
+
+for i in range (2, 11):
+    n = 1.0/i
+    for i in range (1, n):
+        n = (n + 1) * i
+print (n)
