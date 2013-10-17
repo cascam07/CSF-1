7c813c985a9ee15d2aa2c4637ad0058a550a60e9
Bartels <barrob16@ccm-2610-b5.evergreen.edu>
2013-10-17 16:59:01 -0700

    OMG YES I DID IT!!!!!!!!!!!!!!

--- lab3.py
+++ lab3.py
@@ -1,6 +1,22 @@
 n = 100
+b = 1
 a = 0
-for i in range (n):
- a += (3 * i)
- b = a + (3*n)
-print b
+x = 0
+y = 0
+series = 'sum'
+
+if series == 'fibonacci':
+    for i in range (n):
+     a, b= b, a+b
+    print a
+elif series == "sum":
+    for i in range (n):
+     y += (3 * i)
+     x = y + (3*n)
+    print x
+else: 
+ print "Invalid string."
+
+
+
+
