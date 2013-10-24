59c10f8d82dcf4d8f8be678da890b4cdb3aefac6
Bartels <barrob16@ccm-2610-b5.evergreen.edu>
2013-10-24 15:10:49 -0700

    Final HW4.

--- dna_analysis.py
+++ dna_analysis.py
@@ -1,5 +1,5 @@
-# Name: ...
-# Evergreen Login: ...
+# Name: Robin Bartels
+# Evergreen Login: Barrob16
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 3: DNA analysis (Part 1)
@@ -55,6 +55,16 @@ total_count = 0
 gc_count = 0
 at_count = 0
 
+# Individual A, T, G, C Nucleotides present.
+A_nucleotides = 0
+T_nucleotides = 0
+G_nucleotides = 0
+C_nucleotides = 0
+
+# Total A, T, G, C nucleotides
+sum_count = 0
+x = 0
+
 
 # for each base pair in the string,
 for bp in seq:
@@ -66,16 +76,49 @@ for bp in seq:
         # increment the count of at
         at_count = at_count + 1
         
-    # next, if the bp is a G or a C,
     if bp == 'C' or bp == 'G':
-        # increment the count of gc
-        gc_count = gc_count + 1
+       gc_count = gc_count + 1
 
+    if bp == 'A':
+        A_nucleotides = A_nucleotides + 1
+
+    if bp == 'T':
+        T_nucleotides = T_nucleotides + 1
+
+    if bp == 'G':
+        G_nucleotides = G_nucleotides + 1
+    
+    if bp == 'C':  
+        C_nucleotides = C_nucleotides + 1  
+
+
+# Computes AT/GC Ratio
+ATGC_Ratio = float(at_count) / gc_count
 
 # divide the gc_count by the total_count
 gc_content = float(gc_count) / total_count
 at_content = float(at_count) / total_count
 
+# The GC content can be used to categorize microorganisms.
+if float(gc_content) > .60:
+    x = 'high GC content'
+if float(gc_content) < .40:
+    x = 'low GC content'
+if float(gc_content) < .60 and float(gc_content) > .40:
+    x = 'moderate GC content'
+
+
 # Print the answer
 print 'GC-content:', gc_content
-print 'AT-content:' , at_content
+print 'AT-content:', at_content
+print 'G nucleotides:', G_nucleotides
+print 'C nucleotides:', C_nucleotides
+print 'A nucleotides:', A_nucleotides
+print 'T nucleotides:', T_nucleotides
+print 'Sum count:', G_nucleotides + C_nucleotides + A_nucleotides + T_nucleotides
+print 'Total count:', total_count
+print 'seq length:', len(seq)
+print 'AT/GC Ratio:', float(ATGC_Ratio)
+print 'GC Classification:', x
+
+#Riley McGavik conceptulizing DNA, Nucleotides/understanding base pairs/BP's
